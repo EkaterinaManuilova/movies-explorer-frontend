d37032685d5eb8f5ffebe64d7cd883831430ecf6
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-07-01 14:31:59 +0300

    Сверстать секцию Технологии

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -5,6 +5,21 @@ function Techs() {
   return (
     <section className="technology" id="technology">
       <SectionTitle title="Технологии" />
+      <div className="technology__content">
+      <h2 className="technology__title">7 технологий</h2>
+      <p className="technology__subtitle">
+      На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+      </p>
+      <ul className="lechnology__list">
+        <li className="technology__list-item">HTML</li>
+        <li className="technology__list-item">CSS</li>
+        <li className="technology__list-item">JS</li>
+        <li className="technology__list-item">React</li>
+        <li className="technology__list-item">Git</li>
+        <li className="technology__list-item">Express.js</li>
+        <li className="technology__list-item">mongoDB</li>
+      </ul>
+      </div>
     </section>
   )
 }
