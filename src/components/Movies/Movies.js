e29fccefd4b3e71d337e9f304bfa6e7ad280eee5
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-07-05 23:47:04 +0300

    сверстать morebutton

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import MoreButton from '../MoreButton/MoreButton';
 
 function Movies() {
   return (
     <section className="movies">
       <SearchForm />
       <MoviesCardList />
+      <MoreButton />
     </section>
   )
 }
