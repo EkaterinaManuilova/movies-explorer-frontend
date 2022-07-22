94836720c8006458384b39c67ca592ff3dce951c
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-07-23 02:10:31 +0300

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,35 +1,38 @@
 //фильтрация фильмов по длине до 40минут
 export function findShortMovies(movies) {
-  return movies.filter((item) => item.duration < 40);
+    return movies.filter((item) => item.duration < 40)
 }
 
 //филтрация фильмов по ключевому слову и длине
 export function searchAndFilterMovies(movies, keyWord, checkBoxStatus) {
-const queryMovies = Array.isArray(movies) ? movies.filter((item) => {
-  return (item.nameRU).toLowerCase().indexOf(keyWord.toLowerCase()) > -1
-  }
-) : [];
-if (checkBoxStatus) {
-  return findShortMovies(queryMovies);
-}
-return queryMovies
+    const queryMovies = Array.isArray(movies)
+        ? movies.filter((item) => {
+              return (
+                  item.nameRU.toLowerCase().indexOf(keyWord.toLowerCase()) > -1
+              )
+          })
+        : []
+    if (checkBoxStatus) {
+        return findShortMovies(queryMovies)
+    }
+    return queryMovies
 }
 
 //конвертация минут в  часы-минуты
 export function getTimeFromMin(min) {
-const hours = Math.trunc(min/60);
-const minutes = min % 60;
-return `${hours}ч ${minutes}м`;
+    const hours = Math.trunc(min / 60)
+    const minutes = min % 60
+    return `${hours}ч ${minutes}м`
 }
 
 export function changeMovies(movies) {
-  movies.forEach(movie => {
-    if(!movie.image){
-      movie.image = 'https://g2.dcdn.lt/images/pix/kinas-76443525.jpg';
-      movie.thumbnail = 'https://g2.dcdn.lt/images/pix/kinas-76443525.jpg'
-    } else {
-      movie.thumbnail = `https://api.nomoreparties.co${movie.image.formats.thumbnail.url}`
-      movie.image = `https://api.nomoreparties.co${movie.image.url}`
-    }
-  });
-};
+    movies.forEach((movie) => {
+        if (!movie.image) {
+            movie.image = 'https://g2.dcdn.lt/images/pix/kinas-76443525.jpg'
+            movie.thumbnail = 'https://g2.dcdn.lt/images/pix/kinas-76443525.jpg'
+        } else {
+            movie.thumbnail = `https://api.nomoreparties.co${movie.image.formats.thumbnail.url}`
+            movie.image = `https://api.nomoreparties.co${movie.image.url}`
+        }
+    })
+}
